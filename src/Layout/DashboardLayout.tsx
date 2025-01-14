f8171afd033200de76f99add4b529bfafe86c1ea
aayushijain-05 <aayushi.jain@shipglobal.in>
2025-01-14 09:17:52 +0530

    dark mode created and popover for hamburger added

--- src/Layout/DashboardLayout.tsx
+++ src/Layout/DashboardLayout.tsx
@@ -2,6 +2,8 @@ import Home from "@/pages/Home";
 import logo from "../assets/resume-icon.jpg";
 import { Moon, Sun, X, Menu } from "lucide-react";
 import { useState } from "react";
+import { Popover, PopoverContent, PopoverTrigger } from "@/components/ui/popover";
+import { Button } from "@/components/ui/button";
 
 export const DashboardLayout = () => {
   const [isOpen, setIsOpen] = useState(false);
@@ -9,6 +11,10 @@ export const DashboardLayout = () => {
   const buttonStyle = "h-4 pl-2";
 
   const toggleDarkMode = () => {
+    const element = document.getElementById("toggle");
+    if (element) {
+      element.classList.toggle("dark");
+    }
     setDarkMode(!darkMode);
   };
 
@@ -16,10 +22,12 @@ export const DashboardLayout = () => {
     setIsOpen(!isOpen);
   };
   return (
-    <div>
-      <div className={`flex justify-between items-center h-14 bg-gray-100 px-6 py-2.5 fixed left-0 top-0 w-full`}>
+    <div id="toggle" className="h-screen w-screen dark:bg-gray-900">
+      <div
+        className={`flex justify-between items-center h-14 bg-gray-100 dark:bg-black px-6 py-2.5 fixed left-0 top-0 w-full text-black`}
+      >
         <img src={logo} className="h-9 cursor-pointer" />
-        <div className="hidden md:flex md:gap-4 md:items-center md:text-center md:text-sm md:pr-4 md:cursor-pointer">
+        <div className="hidden md:flex md:gap-4 md:items-center md:text-center md:text-sm md:pr-4 md:cursor-pointer text-black dark:text-white">
           <h1 className="hover:text-blue-700">Home</h1>
           <h1 className="hover:text-blue-700">About</h1>
           {darkMode ? (
@@ -28,32 +36,36 @@ export const DashboardLayout = () => {
             <Moon onClick={toggleDarkMode} className={`${buttonStyle}`} />
           )}
         </div>
-        <div className="md:hidden">
+        <div className="md:hidden flex items-center gap-4">
           {darkMode ? (
-            <Sun onClick={toggleDarkMode} className={`${buttonStyle}`} />
+            <Sun onClick={toggleDarkMode} className={`${buttonStyle} dark:text-white`} />
           ) : (
             <Moon onClick={toggleDarkMode} className={`${buttonStyle}`} />
           )}
-        </div>
+        
 
-        <div className="md:hidden">
-          <button onClick={toggleHamburger}>
-            <div className="transition-transform duration-300 ease-in-out ">{!isOpen ? <Menu /> : <X />}</div>
-          </button>
-
+        <Popover>
+          <PopoverTrigger asChild>
+            <Button variant="outline" onClick={toggleHamburger}>
+              <div className="transition-transform duration-300 ease-in-out dark:text-white">
+                {!isOpen ? <Menu /> : <X />}
+              </div>
+            </Button>
+          </PopoverTrigger>
           {isOpen && (
-            <div className="absolute top-full left-0 w-full p-2 bg-gray-100 transition-all duration-300 ease-in-out">
-              <h1 className="hover:text-blue-700">Home</h1>
-              <h1 className="hover:text-blue-700">About</h1>
-            </div>
+            <PopoverContent className=" md:hidden absolute top-full  right-0 w-80 p-2 bg-gray-100 transition-all duration-300 ease-in-out">
+              <div className="md:hidden dark:text-white dark:bg-gary-900">
+                <h1 className="hover:text-blue-700 p-2 ">Home</h1>
+                <h1 className="hover:text-blue-700 p-2 ">About</h1>
+              </div>
+            </PopoverContent>
           )}
+        </Popover>
         </div>
-        </div>
-        <div className={`${isOpen ? 'mt-32' : 'mt-14'}`}>
-    <Home />
-  </div>
-       
       </div>
-    
+      <div className={`${isOpen ? "pt-28 md:pt-14 dark:bg-gray-900" : "pt-14 "}`}>
+        <Home />
+      </div>
+    </div>
   );
 };
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,46 +1,53 @@
 import Image from "../assets/img.png";
 import { Button } from "@/components/ui/button";
 
-export default function Home() {
-  const style = "px-3 py-1.5 bg-slate-100 rounded-md mr-1 text-sm font-medium h-8";
+const Arrays = [
+  { title: "Select a template from our collection." },
+  { title: "Build you resume using our easy to use resume builder." },
+  { title: "Download your resume." },
+];
 
+const style = "px-3 py-1.5 bg-slate-100 rounded-md mr-1 text-sm font-medium h-8 dark:bg-gray-800 dark:text-white";
+export default function Home() {
   return (
-    <div>
+    <>
       {/* main-section */}
 
-      <div className="grid lg:space-x-16 md:grid-cols-2 px-6 py-4 md:py-20 md:px-16 lg:px-32 lg:py-24">
+      <div className="grid lg:space-x-16 lg:grid-cols-2 px-10 py-4 md:py-20 md:px-24 lg:px-28 lg:py-10 dark:bg-gray-900">
         <div>
-          <h1 className="text-3xl sm:text-3xl md:text-6xl lg:text-5xl lg:pt-4  text-center font-semibold">
+          <h1 className="sm:text-3xl md:text-6xl lg:text-5xl lg:pt-4 text-center font-semibold dark:text-white">
             Your resume in three <span className="text-teal-500">easy</span> steps
           </h1>
-          <p className="text-gray-500 text-base sm:text-base md:text-sm font-normal leading-6 mt-8 text-center">
+          <p className="text-gray-500 sm:text-base md:text-sm font-normal leading-6 mt-8 text-center">
             Resume builder tools that assemble well-formatted resume. Through a resume builder, you can create a
             professional-looking resume in a few easy steps. This resume builder offer different template options, so
             you can select the template that best fits your needs and style.
           </p>
-          <div className="leading-10 mt-7">
-            <div className="flex my-2 items-center">
-              <span className={`${style}`}>1</span>
-              <p className="leading-5">Select a template from our collection.</p>
-            </div>
-            <div className="flex my-2 items-center">
-              <span className={`${style}`}>2</span>
-              <p className="leading-5">Build you resume using our easy to use resume builder.</p>
-            </div>
-            <div className="flex my-2 items-center">
-              <span className={`${style}`}>3</span>
-              <p className="leading-5">Download your resume.</p>
-            </div>
+          <div className="leading-10 mt-7 dark:text-gray-400">
+            <Comp />
           </div>
         </div>
 
         <div className="image">
-          <img src={Image} className="h-96 w-full text-center mx-6"/>
-          <Button variant="outline" className="rounded-full bg-teal-500 text-white w-full text-sm m-6">
+          <img src={Image} className="h-96 w-full text-center mx-6" />
+          <Button variant="outline" className="rounded-full bg-teal-500 text-white dark:text-black w-full text-sm m-6">
             Select Template
           </Button>
         </div>
       </div>
-    </div>
+    </>
   );
 }
+
+const Comp = () => {
+  return (
+    <div>
+      {Arrays.map((item, index) => (
+        <div key={index} className="flex my-2 items-center">
+          <span className={`${style}`}>{index + 1}</span>
+          <p className="leading-5">{item.title}</p>
+        </div>
+      ))}
+    </div>
+  );
+};
