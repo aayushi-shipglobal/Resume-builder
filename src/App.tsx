4266536f1692b80e4d9d11b6f021e9b5b63f91cc
aayushijain-05 <aayushi.jain@shipglobal.in>
2025-01-31 15:37:15 +0530

    setup done

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,7 @@
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
-import Routes from "./routes/Routes";
 
-function App() {
-  const router = createBrowserRouter([Routes()]);
-  return <RouterProvider router={router} />;
+
+export const App = () => {
+  return (
+    <div>App</div>
+  )
 }
-
-export default App;
